7862ef5be72e60c2b2ec3d1e316a006497763e02
Robert Blacha <rb97@njit.edu>
2025-03-07 14:44:38 -0500

    Complete redesign of the model following autoencoder standards, as well as simple verificatoin that the actual model will get better at reading the data.

--- main.py
+++ main.py
@@ -1,41 +1,107 @@
 import torch
-import torch.nn as nn
+from torch import nn, distributions, exp, log
 import numpy as np
 import pandas as pd
 from datetime import timedelta
 
 totalSyscalls = 457
+emb_dim = 100 # HyperParameter
+seq_len = 2000 # HyperParameter
+
 class AIAV(nn.Module):
-    def __init__(self):
+
+    class Encoder(nn.Module):
+        def __init__(self, seq_len, emb_dim):
+            super().__init__()
+            self.inp = totalSyscalls + 1
+            self.seq_len = seq_len
+            self.emb_dim = emb_dim
+            self.model = nn.LSTM(
+                input_size=self.inp,
+                hidden_size=self.emb_dim,
+                num_layers=1,
+                batch_first=True # Tensors will be (batch, seq, features)
+            )
+
+        def encode(self, x):
+            x = x.reshape((1, self.seq_len, self.inp))
+            final, (hidden, cells) = self.model(x)  # Outputs Final, (hidden, cells)
+            # We want the hidden layer because we will be reconstructing the model from it
+            # Hidden is a 1xemb_dim torch tensor. We will be using this to reconstruct the input
+            return hidden.squeeze()
+
+    class Decoder(nn.Module):
+        def __init__(self, seq_len, emb_dim):
+            super().__init__()
+            self.inp = emb_dim # Takes the hidden layer of decoder as input
+            self.seq_len = seq_len
+            self.emb_dim = totalSyscalls + 1
+            self.model = nn.LSTM(
+                input_size=self.inp,
+                hidden_size=self.emb_dim,
+                num_layers=1  # HyperParameter
+            )
+
+        def decode(self, x):
+            x = x.repeat(self.seq_len, 1) # For every sequence
+            x, (_, _) = self.model(x) # x now is the reconstructed representations of the sequences
+            return x
+
+    def __init__(self, seq_len, emb_dim):
         super().__init__()
-        self.layer1 = nn.RNN(totalSyscalls+1, totalSyscalls)
-        # RNN handles the tanh linearity, could consider switching to ReLu
-        self.layer2 = nn.Linear(totalSyscalls, 1)
-        self.act2 = nn.Sigmoid()
-        self.hidden = torch.zeros(1, totalSyscalls) # Free to add more layers but probably not?
+        self.encoder = self.Encoder(seq_len, emb_dim)
+        self.decoder = self.Decoder(seq_len, emb_dim)
+
 
     def forward(self, x):
-        x, self.hidden = self.layer1(x, self.hidden)
-        #x = self.act1(x) TANH already RNN'd
-        x = self.act2(self.layer2(x))
-        return x
+        hidden = self.encoder.encode(x) # Outputs Final, (hidden, cells)
+        out = self.decoder.decode(hidden)
+        return out
 
+def getData(filename):
+    data = pd.read_csv(filename)
+    data = data.drop(columns=['PID'])
+    data['TIMESTAMP'] = pd.to_datetime(data['TIMESTAMP'])
+    data['TIMESTAMP'] = data['TIMESTAMP'].diff().fillna(timedelta(0)).apply(lambda x: x / timedelta(microseconds=1))
+    syscalls = pd.DataFrame(columns=range(458), index=range(len(data)))
+    syscalls = syscalls.fillna(0)
+    for i in range(len(data)):
+        syscalls.iloc[i, [data['SYSCALL_NUMBER'][i]]] = 1
+        syscalls.iloc[i, -1] = data['TIMESTAMP'][i]
+    return syscalls
 
+if __name__=="__main__":
+    rnn = AIAV(seq_len, emb_dim)
+    data = getData('zpoline_syscalls.csv')
+    inp = torch.tensor(data.head(seq_len).values.astype(np.float32)).unsqueeze(0) # Creates the 'batch layer'
 
-rnn = AIAV()  # consider changing nonlinearity to relu
+    # Training Doers
+    optimizer = torch.optim.Adam(rnn.parameters(), lr=1e-3) # lr is a HyperParameter, but not that important as long as nothing "explodes"
+    lossFunc = nn.L1Loss(reduction='sum') # Sum differences
 
-data = pd.read_csv('zpoline_syscalls.csv')
-data = data.drop(columns=['PID'])
-data['TIMESTAMP'] = pd.to_datetime(data['TIMESTAMP'])
-data['TIMESTAMP'] = data['TIMESTAMP'].diff().fillna(timedelta(0)).apply(lambda x: x / timedelta(microseconds=1))
-syscalls = pd.DataFrame(columns=range(458), index=range(len(data)))
-syscalls = syscalls.fillna(0)
-for i in range(len(data)):
-    syscalls.iloc[i, [data['SYSCALL_NUMBER'][i]]] = 1
-    syscalls.iloc[i, -1] = data['TIMESTAMP'][i]
-print(syscalls.head(10))
-inp = torch.tensor(syscalls.head(10).values.astype(np.float32))
-print(rnn.forward(inp))
-
+    ls = []
+    testls = []
+    rnn.train()
+    for i in range(1,(data.shape[0] // seq_len)):
+        inp = torch.tensor(data.iloc[i*seq_len:(i+1)*seq_len, :].values.astype(np.float32)).unsqueeze(0)
+        out = rnn.forward(inp)
+        inp = inp.squeeze() # Undoes the batch layer
+        loss = lossFunc(out, inp)
+        optimizer.zero_grad() # If not included, as this loops previous grads will leak into next update
+        loss.backward() # Calculate the gradients
+        optimizer.step() # Update the weights
+        ls.append(loss.item())
+    rnn.eval()
+    with torch.no_grad():
+        for i in range(1, (data.shape[0] // seq_len)):
+            inp = torch.tensor(data.iloc[i * seq_len:(i + 1) * seq_len, :].values.astype(np.float32)).unsqueeze(0)
+            out = rnn.forward(inp)
+            inp = inp.squeeze()  # Undoes the batch layer
+            loss = lossFunc(out, inp)
+            testls.append(loss.item())
+    train_loss = np.mean(ls)
+    test_loss = np.mean(testls)
+    print(train_loss, test_loss)
 #TODO: Train!
+#TODO: Write the train/test split
 
